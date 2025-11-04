b456459e0b8e80a43b3f90e79cb4b5d312ca2f3a
robinliu <68115265+RobinLiu69@users.noreply.github.com>
2025-11-05 04:22:40 +0800

    Refactor main function and add win condition
    Moved world initialization inside the main function and added a win condition check.

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from core.world import World
 pygame.init()
 screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("The Great Bio Arena")
-world = World(screen)
 clock = pygame.time.Clock()
 
 def main():
+    world = World(screen)
     run = True
     while run:
         dt = clock.tick(20) / 1000.0
@@ -18,6 +18,8 @@ def main():
         world.update(dt)
         world.draw()
         pygame.display.flip()
+
+        if world.win: break
     
     pygame.quit()
                 
@@ -25,3 +27,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
