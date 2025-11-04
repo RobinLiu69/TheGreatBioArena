987d1cb5d38ba654c85e5d1f6b23a95c14200948
Angusinttsh <163747106+Angusinttsh@users.noreply.github.com>
2025-11-04 18:37:09 +0800

    Refactor move_logic to use dictionary for note

--- creatures/Killer.py
+++ creatures/Killer.py
@@ -11,14 +11,14 @@ class Killer(BaseCreature):
 
     def move_logic(self, energy_ratio, note) -> tuple[int, int]:
         if len(note) == 0:
-            note.append(random.randint(0, 360))
-            note.append(0)
-        elif note[1]==20:
-            note[1] = 0
-            note[0] = random.randint(0, 360)
+            note["方向"] = random.randint(0, 360)
+            note["時間"] = 0
+        elif note["時間"]==20:
+            note["時間"] = 0
+            note["方向"] = random.randint(0, 360)
         else:
-            note[1] +=1
-        return note[0], 0.2
+            note["時間"] +=1
+        return note["方向"], 0.2
 
     def reproduce_logic(self, energy_ratio, note) -> list[int]:
         if energy_ratio > 5:
