0164925c5e09db58f518d8155eb6f28e5385e7e9
robinliu <68115265+RobinLiu69@users.noreply.github.com>
2025-11-10 14:54:16 +0800

    Update Knight.py

--- creatures/Knight.py
+++ creatures/Knight.py
@@ -4,15 +4,25 @@ from core.base_parts import *
 
 class Knight(BaseCreature):
     def init_parts(self) -> list[BasePart]:
-        return [NucleusCellulae()] + [Lorica() for _ in range(2)] + [Spiculum() for _ in range(4)] + [Folium() for _ in range(8)] + [Cytoplasma()]
+        return [NucleusCellulae()] + [Lorica() for _ in range(3)] + [Spiculum() for _ in range(6)] + [Folium() for _ in range(4)] + [CytoplasmaMagnum()]
 
     def get_color(self) -> tuple[int, int, int]:
         return (148, 148, 73)
 
     def move_logic(self, energy_ratio, note) -> tuple[int, int]:
-        return 0, 0
+        if energy_ratio < 2:
+            if len(note) == 1:
+                note["方向"] = 45+(random.randint(0, 3)*90)
+                note["速度"] = 1
+            note["速度"] -= 0.002
+            return note["方向"], note["速度"]
+        else:
+            if len(note) == 1 or note["方向"] ==360:
+                note["方向"] = 0
+            note["方向"] += 1
+            return note["方向"], 0.2
 
     def reproduce_logic(self, energy_ratio, note) -> list[int]:
-        if energy_ratio > 4.3:
+        if energy_ratio > 6:
             return [3, 1]
         return []
